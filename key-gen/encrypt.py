2584105a1f7b6cf0b8ea1caea080fd9e2ac710d2
Luke Brady <lbrad23105@gmail.com>
2018-11-02 13:31:13 -0400

    Working on the encryption function.
    Signed-off-by: Luke Brady <lbrad23105@gmail.com>

--- key-gen/encrypt.py
+++ key-gen/encrypt.py
@@ -2,8 +2,30 @@
 
 import sys, base58
 
-key = sys.stdin.readline()
+# Decode the supplied key to encrypt the given message.
+def decodeKey(key):
+    decode = base58.b58decode(key.strip())
+    intKey = int.from_bytes(decode, byteorder='big')
+    return intKey
 
-decode = base58.b58decode(key.strip())
+# Encrypt the supplied message with the supplied key.
+def encryptMessage(message, key):
+    msg = ([(ord(char) * key) for char in message])
+    print(msg)
+# Get the key from stdin, decode the key, and encrypt
+# the supplied message. Exit with code=1 if message
+# is not supplied to the program.
+def main():
+    if len(sys.argv) < 2:
+        sys.stdin.readline()
+        print('A message was not supplied.')
+        print('Exiting...')
+        exit(code=1)
+    else:
+        key = sys.stdin.readline().strip()
+        intKey = decodeKey(key)
+        encryptMessage(sys.argv[1], intKey)
+        exit(code=0)
 
-print(decode)
+if __name__ == '__main__':
+    main()
